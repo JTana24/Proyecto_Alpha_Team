b97f1d3f9e1ce25ef1a55565c9725b807ab8157d
Aldo Burgos <56781359+aldoCR91@users.noreply.github.com>
2025-02-04 12:14:10 -0600

    Listos los modelos

---
+++ Proyecto_Alpha_Team/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Data.Entity;
+
+namespace Proyecto_Alpha_Team.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection") { }
+
+        public DbSet<TipoIdentificacion> TipoIdentificaciones { get; set; }
+    }
+}
---
+++ Proyecto_Alpha_Team/Models/TipoIdentificacion.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_Alpha_Team.Models
+{
+    [Table("tipo_identificacion")]
+    public class TipoIdentificacion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Nombre { get; set; }
+    }
+}
